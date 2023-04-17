7f86e3ea2117960510888df7b838a73424e4bf8f
xltan98 <xinnli98@gmail.com>
2023-04-17 20:02:25 +0800

    intial

---
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -0,0 +1,18 @@
+package sg.edu.nus.iss;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        List <String> shoppingCart= new ArrayList<>();
+        Scanner scan = new Scanner (System.in);
+
+    }
+}
